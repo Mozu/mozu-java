afe0bca6ee84f7165703194f8acca1bec54e438c
John Gatti <john_gatti@volusion.com>
2014-11-07 10:47:48 -0600

    Set version numbers to 1.13.0

--- mozu-java-core/src/main/java/com/mozu/api/MozuClientFactory.java
+++ mozu-java-core/src/main/java/com/mozu/api/MozuClientFactory.java
@@ -11,6 +11,12 @@ public class MozuClientFactory<TResult>  {
         return (MozuClient<?>) constructor.newInstance(clz);
     }
 
+    public static MozuClient<?> getInstance(Class<?> clz, String className) throws Exception {
+        Constructor<? extends MozuClient> constructor =
+                (Constructor<? extends MozuClient>) Class.forName(className).getConstructor(new Class[] {Class.class});
+        return (MozuClient<?>) constructor.newInstance(clz);
+    }
+
     public static MozuClient<?> getInstance() throws Exception {
         Constructor<? extends MozuClient> constructor = 
                 (Constructor<? extends MozuClient>) Class.forName(CLIENT_CLASS_NAME).getConstructor();
