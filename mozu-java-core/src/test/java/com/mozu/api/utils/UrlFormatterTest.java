1422f72193c0ada8ae82016f9c5c50242dcda3ca
bob_hewett <bob_hewett@L02968.corp.volusion.com>
2016-03-02 16:19:04 -0600

    Support DateTime parameters and commit to ISO Date Time String

--- mozu-java-core/src/test/java/com/mozu/api/utils/UrlFormatterTest.java
+++ mozu-java-core/src/test/java/com/mozu/api/utils/UrlFormatterTest.java
@@ -2,6 +2,8 @@ package com.mozu.api.utils;
 
 import static org.junit.Assert.*;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.junit.Test;
 
 public class UrlFormatterTest {
@@ -42,5 +44,15 @@ public class UrlFormatterTest {
         formatter.formatUrl("searchFilter", "ratings > 3.5");
         
         assertEquals ("/api/commerce/catalog/storefront/products/Lowrise%20pants-002/locationinventory?searchfilter=ratings%20%3E%203.5", formatter.getResourceUrl());
+    }
+    @Test
+    public void testUrlFormatDateTime () {
+        UrlFormatter formatter = new UrlFormatter("/api/commerce/catalog/storefront/products/{productId}/locationinventory?filter={filter}&startTime={startTime}&searchFilter={searchFilter}")  ;
+        formatter.formatUrl("productId", "Lowrise pants-002");
+        formatter.formatUrl("filter", null);
+        formatter.formatUrl("startTime", new DateTime(2016, 3, 2, 15, 39, DateTimeZone.UTC));
+        formatter.formatUrl("searchFilter", null);
+        
+        assertEquals ("/api/commerce/catalog/storefront/products/Lowrise%20pants-002/locationinventory?starttime=2016-03-02T15%3A39%3A00.000Z", formatter.getResourceUrl());
     }
 }
